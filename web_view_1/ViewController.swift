824b9a4e067a715b71412569ef32d68bb08f2ae5
taeyoonL <tylee3715@gmail.com>
2023-07-07 13:10:52 +0900

    Initial Commit

---
+++ web_view_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  web_view_1
+//
+//  Created by 이태윤 on 2023/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
